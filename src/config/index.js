4251e116199bcf86d3ba8e7efc61129e3bd24e9a
Varik Matevosyan <varikmatevosyan@gmail.com>
2017-06-02 00:15:36 -0700

    api key removed

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const YOUTUBE_API_KEY = "AIzaSyD0et33Ph6ZvGzhOp1V8alEj5CtDP5JncY";
+const YOUTUBE_API_KEY = "YOUR_API_KEY";
 export default {
   API_URL: "http://52.232.85.160/api?vid=",
   SEARCH_API_URL: `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&type=video&part=snippet&maxResults=40&q=`
